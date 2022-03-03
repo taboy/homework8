cbc5912d4532265a85a7653ebee0891f3ccfa251
a a <t.mirzeyev@mail.com>
2022-03-03 09:52:08 +0300

    finish

--- 63/Program.cs
+++ 63/Program.cs
@@ -1,14 +1,18 @@
 ﻿// Задача 63: Сформировать трёхмерный массив с не повторяющимися двузначными числами и вывести его на экран построчно, с индексами элементов.
 int[,,] array = new int[3, 4, 5];
+int s=10;
 for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int n = 0; n < array.GetLength(1); n++)
     {
         for (int j = 0; j < array.GetLength(2); j++)
+
         {
-            array[i, n, j] = new Random().Next(1, 10);
+            
+            array[i, n, j] = s;
+            s++;
         }
-        Console.WriteLine();
+        
     }
 }
 
@@ -20,9 +24,9 @@ for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(2); j++)
         {
-            Console.Write(array[i, n, j] + " ");
+            Console.Write($"({i}{n}{j})-{array[i, n, j]}");
         }
         Console.WriteLine();
     }
-     Console.WriteLine();
+    Console.WriteLine();
 }
