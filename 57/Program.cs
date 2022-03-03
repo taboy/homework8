866340f35e3830b05f0444635acfc91a5ed3757a
a a <t.mirzeyev@mail.com>
2022-03-03 17:03:31 +0300

    finish

--- 57/Program.cs
+++ 57/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 57: Написать программу, которая упорядочивает по убыванию элементы каждой строки двумерного массива.
 
-int[,] array = new int[10, 10];
+int[,] array = new int[5, 5];
 for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
@@ -28,7 +28,7 @@ for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int n = j + 1; n < array.GetLength(1); n++)
 
-            if (array[i, n] < array[i, j ])
+            if (array[i, n] > array[i, j ])
             {
                 int tmp = array[i, j];
                 array[i, j] = array[i, n ];
